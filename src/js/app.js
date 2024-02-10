7190e5e1ac2ae82877640429c18b3cddfe8a6fc0
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-02-10 23:43:09 +0800

    added some of the files and functionality

---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+import { createElement } from './utils';
+import { initRouter } from './router';
+
+function Header(mainDiv) {
+
+    const siteIcon = createElement('img', {
+        src: '../images/pets.webp', alt: 'Dog and Cat Icon'
+    });
+
+    const appTitle = createElement('h1', {
+        textContent: 'Dog, Cat, and Weather',
+        className: 'heading',
+    });
+
+    // nav items
+    const home = createElement('a', {
+        href: '/#/home',
+        textContent: 'HOME',
+    });
+    const dog = createElement('a', {
+        href: '/#/dog',
+        textContent: 'DOG',
+    });
+    const cat = createElement('a', {
+        href: '/#/cat',
+        textContent: 'CAT',
+    });
+
+    const weather = createElement('a', {
+        href: '/#/weather',
+        textContent: 'WEATHER',
+    });
+
+    const join = createElement('a', {
+        href: '/#/join',
+        textContent: 'JOIN',
+    });
+
+    const nav = createElement('nav', {}, [home, dog, cat, weather, join]);
+
+    return createElement('header', {}, [ siteIcon, appTitle, nav]);
+}
+
+function Footer() {
+    const copyright = createElement('span', {
+        textContent: `Copyright © ${new Date().getFullYear()}`,
+    });
+
+    return createElement('footer', {}, [copyright]);
+}
+
+function App() {
+    const main = createElement('main', {}, []);
+
+    initRouter(main);
+
+    return createElement('div', {}, [Header(main), main, Footer()]);
+}
+
+export default App;
