7c0014c6246c9bba8b884eeceb5938462a2864e4
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-02-11 23:12:03 +0800

    added the weatherIcon

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,4 @@
-import { createElement, fetchData, getLocation } from './utils';
+import { createElement, fetchData, getLocation, formatTime, capitalize } from './utils';
 
 const catApiKey = process.env.CAT_API_KEY;
 const dogApiKey = process.env.DOG_API_KEY;
@@ -10,17 +10,16 @@ async function home() {
             src: 'https://raw.githubusercontent.com/fireburn553/dog-cat-and-weather/main/src/images/close-up-portrait-beautiful-cat.webp', 
             alt: 'Dog and Cat Hero Image',
             className: "hero-image"
-        });  
+        }); 
         const position = await getLocation();
-        const weatherData = await fetchData(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${weatherApiKey}`)
-        console.log(weatherData)
+        const weatherData = await fetchData(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${weatherApiKey}&units=metric`);
         const weatherElement = displayWeatherInfo(weatherData);
 
         const catData = await fetchData('https://api.thecatapi.com/v1/images/search?limit=1', catApiKey);
         const dogData = await fetchData('https://api.thedogapi.com/v1/images/search?limit=1', dogApiKey);
 
-        const catElement = imageDiv(catData, 'cat-random-image', 'Random Cat Image Today! Enjoy');
-        const dogElement = imageDiv(dogData, 'dog-random-image', 'Random Dog Image Today! Enjoy');
+        const catElement = imageDiv('Cat', catData, 'cat-random-image', 'Random Cat Image Today! Enjoy');
+        const dogElement = imageDiv('Dog', dogData, 'dog-random-image', 'Random Dog Image Today! Enjoy');
 
         const title = createElement('h2', { textContent: 'HOME' });
 
@@ -31,7 +30,10 @@ async function home() {
     }
 }
 
-export function imageDiv(data, className, text) {
+export function imageDiv(category, data, className, text) {
+    const cardTitle = createElement('h3', {
+        textContent: category
+    })
     const picture = createElement('img', {
         src: data[0].url,
         alt: data[0].id,
@@ -42,20 +44,43 @@ export function imageDiv(data, className, text) {
         textContent: text,
     });
 
-    const container = createElement('div', { className: 'element-container' }, [picture, label]);
-
-    return container;
+    return createElement('div', { className: 'element-container' }, [cardTitle, picture, label]);
 }
 
 function displayWeatherInfo(weatherData) {
-    const temperature = weatherData.main.temp;
+    
+    const placeName = weatherData.name;
     const description = weatherData.weather[0].description;
-
-    const weatherInfoElement = createElement('p', {
-        textContent: `Temperature: ${temperature}°C, Description: ${description}`,
+    const sunrise = formatTime(weatherData.sys.sunrise)
+    const sunset = formatTime(weatherData.sys.sunset)
+    
+    const cardTitle = createElement('h3', {
+        textContent: 'Weather Lookout'
+    })
+    
+    const iconWeather = createElement('img', {
+        src: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`,
+        alt: description + 'icon',
+        className: 'icon-weather',
     });
 
-    return weatherInfoElement;
+    const weatherInfoElement = createElement('p', {
+        textContent: `Temperature: ${weatherData.main.temp}°C`,
+        className: 'element'
+    });
+
+    const place = createElement('h4', {
+        textContent: placeName.toUpperCase()
+    })
+    const weatherDesc = createElement('p', {
+        textContent: capitalize(description)
+    })
+
+    const sunRiseSet = createElement('p', {
+        textContent: `Sunrise: ${sunrise} | Sunset: ${sunset}`
+    })
+
+    return createElement('div', {className: 'element-container'}, [cardTitle, place, iconWeather,weatherDesc, weatherInfoElement, sunRiseSet]);
 }
 
 export default home;
